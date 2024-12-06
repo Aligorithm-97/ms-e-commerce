c29922c6da7fe3fa21f53d8a17f62c87673b89d3
ali <atemurtas97@gmail.com>
2024-12-06 11:24:01 +0300

    aligorithm-msUpdateCustomer

--- services/customer/src/main/java/com/aligorithm/ecommerce/customer/CustomerService.java
+++ services/customer/src/main/java/com/aligorithm/ecommerce/customer/CustomerService.java
@@ -1,7 +1,11 @@
 package com.aligorithm.ecommerce.customer;
 
+import com.aligorithm.ecommerce.exception.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +16,29 @@ public class CustomerService {
     public String createCustomer(CustomerRequest request) {
         var customer = repository.save(mapper.toCustomer(request));
         return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest request) {
+        var customer = repository.findById(request.id())
+                .orElseThrow(()->new CustomerNotFoundException(
+                       format("Customer not found with  :: id :: %s",request.id())
+                ));
+        mergeCustomer(customer,request);
+        repository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstname())){
+            customer.setFirstname(request.firstname());
+        }
+        if (StringUtils.isNotBlank(request.lastname())){
+            customer.setLastname(request.lastname());
+        }
+        if (StringUtils.isNotBlank(request.email())){
+            customer.setEmail(request.email());
+        }
+        if (request.address() != null){
+            customer.setAddress(request.address());
+        }
     }
 }
