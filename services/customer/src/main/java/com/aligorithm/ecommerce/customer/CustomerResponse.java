f79215b6f3b97475c9c933cfa304ba7a4e5bf102
ali <atemurtas97@gmail.com>
2024-12-06 15:36:10 +0300

    aligorithm-msProductServiceWithoutDbConnection

--- services/customer/src/main/java/com/aligorithm/ecommerce/customer/CustomerResponse.java
+++ services/customer/src/main/java/com/aligorithm/ecommerce/customer/CustomerResponse.java
@@ -1,8 +1,5 @@
 package com.aligorithm.ecommerce.customer;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-
 public record CustomerResponse (
         String id,
         String firstname,
---
+++ services/product/src/main/java/com/aligorithm/ecommerce/handler/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.aligorithm.ecommerce.handler;
+
+import java.util.Map;
+
+public record ErrorResponse(
+    Map<String, String> errors
+) {
+
+}
