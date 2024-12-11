0d91fdeb3c38e5fc113b3059b545a43976a66110
ali <atemurtas97@gmail.com>
2024-12-11 14:03:58 +0300

    aligorithm-orderMicroService

--- services/order/src/main/java/com/aligorithm/ecommerce/orderline/OrderLineController.java
+++ services/order/src/main/java/com/aligorithm/ecommerce/orderline/OrderLineController.java
@@ -1,7 +1,26 @@
 package com.aligorithm.ecommerce.orderline;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/order-lines")
 public class OrderLineController {
+
+    private final OrderLineService service;
+
+    public OrderLineController(OrderLineService service) {
+        this.service = service;
+    }
+    @GetMapping("/order/{order-id}")
+    public ResponseEntity<List<OrderLineResponse>> findByOrderId(
+            @PathVariable("order-id") Integer orderId
+    ) {
+        return ResponseEntity.ok(service.findAllByOrderId(orderId));
+    }
 }
--- services/order/src/main/java/com/aligorithm/ecommerce/orderline/OrderLineService.java
+++ services/order/src/main/java/com/aligorithm/ecommerce/orderline/OrderLineService.java
@@ -3,6 +3,9 @@ package com.aligorithm.ecommerce.orderline;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class OrderLineService {
 
@@ -17,5 +20,12 @@ public class OrderLineService {
     public Integer saveOrderLine(OrderLineRequest orderLineRequest) {
         var order = mapper.toOrderLine(orderLineRequest);
         return repository.save(order).getId();
+    }
+
+    public List<OrderLineResponse> findAllByOrderId(Integer orderId) {
+        return repository.findAllByOrderId(orderId)
+                .stream()
+                .map(mapper::toOrderLineResponse)
+                .collect(Collectors.toList());
     }
 }
